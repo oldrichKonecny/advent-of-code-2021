d112eab049bad782b0d0c6756acb1fcd95e7e202
oldrich <oldik.kon@gmail.com>
2021-12-26 10:51:34 +0100

    WIP day 19, 2O, 21 init

---
+++ puzzles/day19/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let input = include_str!("../input_tst.txt");
+
+    println!("First solution: {}", first_solution(&input));
+    println!("Second solution: {}", second_solution(&input));
+}
+
+fn second_solution(input: &str) -> usize {
+    0
+}
+
+fn first_solution(input: &str) -> usize {
+    0
+}
+
+
+mod tests {
+    #[test]
+    fn test1() {
+        assert_eq!(2, 1 + 1);
+    }
+}
+
+
